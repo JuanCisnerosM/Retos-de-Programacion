bd4fd7302def6927e1f98ac105f8298c016e17d4
JuanCisnerosM <juan.cisnerosmtz@hotmail.com>
2025-11-29 15:02:45 -0600

    feat(java/aprendiendospring): com.aprendiendospring.creandoapisconsrping

---
+++ Java/Proyectos/creandoapisconspring/src/main/java/com/aprendiendospring/creandoapisconspring/CreandoapisconspringApplication.java
@@ -0,0 +1,13 @@
+package com.aprendiendospring.creandoapisconspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreandoapisconspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreandoapisconspringApplication.class, args);
+	}
+
+}
---
+++ Java/Proyectos/creandoapisconspring/src/test/java/com/aprendiendospring/creandoapisconspring/CreandoapisconspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendiendospring.creandoapisconspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreandoapisconspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
