895d7df71beedbe45b69c7afc382512a148b92e8
JuanCisnerosM <juan.cisnerosmtz@hotmail.com>
2025-12-06 16:51:36 -0600

    refactor(java1): se renombro la carpeta del curso a Java1
    Se renombro la carpeta del curso a Java 1 para evitar problemas con el VS Code a la hora de correr
    los archivos (error: no encontraba el PATH)

---
+++ Java/Cursos/Java1/Conditionals.java
@@ -0,0 +1,17 @@
+public class Conditionals {
+
+    public static void main(String[] args) {
+        
+        // Condicionales 
+        /**
+         * Java peromite tomar un camino u otro dependiednto la condicion
+         */
+        var age = 4;
+
+        if (age >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+    }
+}
