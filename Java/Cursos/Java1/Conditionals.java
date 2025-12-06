a9bf1443761d222c746793d622972bb08c59f86f
JuanCisnerosM <juan.cisnerosmtz@hotmail.com>
2025-12-06 17:38:47 -0600

    feat(conditionals): uso de los condicionales (if-else) (if-esle if-else) y el switch

--- Java/Cursos/Java1/Conditionals.java
+++ Java/Cursos/Java1/Conditionals.java
@@ -2,16 +2,63 @@ public class Conditionals {
 
     public static void main(String[] args) {
         
-        // Condicionales 
-        /**
-         * Java peromite tomar un camino u otro dependiednto la condicion
-         */
-        var age = 4;
+        // * Condicionales 
 
-        if (age >= 18) {
+        // * if
+
+        /**
+         * Java peromite tomar un camino u otro dependiednto la condicion (if)
+         * (if) si ..... (si se cumple)
+         * (else) entonces... (si no se cumple entonces )
+         * 
+         * (if) si ..... (si se cumple)
+         * (else if) entonces si... (si no se cumple la 1ra condicion ntonces .... si se umple esto....)
+         * (else) emtpmces... (si no se umple la 2da condicion entonces .....)
+         */
+        var age = 18;
+
+        if (age > 18) {
             System.out.println("Es mayor de edad");
+        } else if (age == 18) {
+            System.out.println("Apenas acaba de cumplir 18");
         } else {
             System.out.println("Es menor de edad");
         }
+
+        // * Switch
+        /**
+         * Diferentes casos
+         * Mas optimo en tiempo de ejecucion al comprobar varias condicones o casos
+         */
+
+        var day = 6;
+
+        switch (day) {
+            case 1:
+                System.out.println("es Lunes");
+                break;
+            case 2:
+                System.out.println("es Martes");
+                break;
+            case 3:
+                System.out.println("es Miercoles");
+                break;
+            case 4:
+                System.out.println("es Jueves");
+                break;
+            case 5:
+                System.out.println("es Viernes");
+                break;
+            default:
+                System.out.println("No es dia habil");
+                break;
+        }
+        
+        /**
+         * ! Cuando usar un (if) y cuando usar un (switch) ?
+         * Usa if para evaluar condiciones complejas y rangos (booleanas, comparaciones) 
+         * y switch para seleccionar entre múltiples valores fijos de una misma variable (enteros, caracteres, enums) para mejorar legibilidad y eficiencia, 
+         * ya que switch es más rápido que muchos if-else if anidados, siendo el if más versátil para lógica general y switch ideal para menús o estados específico
+         */
     }
 }
