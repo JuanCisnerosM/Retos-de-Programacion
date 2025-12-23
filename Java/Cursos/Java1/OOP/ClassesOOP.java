5d6232fb9e724d7fa3c1c7d63b721318bf8279fd
JuanCisnerosM <juan.cisnerosmtz@hotmail.com>
2025-12-23 12:15:20 -0600

    feat(OOP): Principios de la Programacion Orientada a Objetos
    Creacion de clases, e implementacion de esos metodos de clase en otro archivo
    
    Clase (El Molde): Es la definición o plantilla para crear objetos. Define qué atributos y métodos
    tendrá un tipo específico de objeto (ej. Clase Perro).
    
    Objeto (La Cosa Real): Es una instancia concreta de una clase, con sus propios valores para los
    atributos (ej. mi_perro_ozzy, un objeto de la clase Perro, con nombre "Ozzy" y edad 2).
    
    Atributos (Características): Los datos o propiedades que describen a un objeto (ej. color, tamaño,
    nombre).
    
    Metodos (Acciones): Las funciones o comportamientos que un objeto puede realizar (ej. ladrar(),
    correr(), arrancar()).

--- Java/Cursos/Java1/OOP/ClassesOOP.java
+++ Java/Cursos/Java1/OOP/ClassesOOP.java
@@ -3,12 +3,41 @@ package OOP;
 public class ClassesOOP {
     
     public static void main(String[] args) {
-        
-        var persona = new Person();
 
-        persona.name = "Giorno";
-        persona.age = 24;
+        var persona = new Person("GiornoG iovana", 23);
+
+        /**
+         * Al momento de crear al Metodo Constructor nos obliga a meter los "parametros" dentro de la instancia
+         * por lo uq eno podemos dejarlo como estaba antes:
+         * 
+         * var persona = new Person();
+         * 
+         * Ya que el constructor es el que "dice" que parametros debe llevar. en este caso en Person.java
+         * el Constructor nos "pide" un nombre y una edad 
+         * 
+         * public Person(String name , int age) {
+         *     this.name = name;
+         *     this.age = age;
+         * 
+         * }
+         * 
+         * Entonces a nuestra variable se le debe pasar esos parametros que "dicta" el contructor
+         * 
+         * var persona = new Person("GiornoGiovana", 23);
+         * 
+         */
+        // persona.name = "Giorno";
+        // persona.age = 24;
 
         persona.sayHello();
+
+        persona.name = "Dio Brando";
+
+        System.out.println(persona.name);
+
+        // Usar el "molde" de Person para reutilizar el objeto.
+        // Creamos a la peronsa2 y llamamos al metodo sayHello(); de Person
+        var persona2 = new Person("Jonathan Joestar", 35);
+        persona2.sayHello();
     }
 }
--- Java/Cursos/Java1/OOP/Person.java
+++ Java/Cursos/Java1/OOP/Person.java
@@ -2,11 +2,39 @@ package OOP;
 
 public class Person {
     
-    // Atributos
-    String name; // Atributo nombre
+    // * Atributos
+    /**
+     * Son las Variables que definen el estado, caracteristricas del objeto.
+     */
+    String name; // Atributo nombre 
     int age; // Atributo edad
 
-    // Metodo
+    // * Metodo Constructor
+    /**
+     * Es un mecanismo de inicializacion
+     */
+    public Person(String name, int age) {
+        // Establecer la ralacion con las propiedades internas de la clase, this.name hace referencia a los Atributos de la clase y name hace referencia al constructor
+        this.name = name;
+        // Establecer la ralacion con las propiedades internas de la clase, this.age hace referencia a los atributos de la clase, y age hace referencia al contrunctor
+        this.age = age;
+        /**
+         * ! Yo aqui pudieroa cambiar el nombre de las variables del contructor pero las buenas practicas dice que deben de llevar el mismo nombre
+         * Siendo esto valido pero sin buenas practicas:
+         * 
+         * public Person (String miNombre, int miEdad){
+         *     this.name = miNombre;
+         *     this.age = miEdad;
+         * }
+         * 
+         * 
+         *  */
+    }
+
+    // * Metodos
+    /**
+     * Son las Funciones quie definen el comportamiento del objeto.
+     */
     public void sayHello() {
         System.out.println("Hola soy " + name + " y tengo " + age + " años");
     }
