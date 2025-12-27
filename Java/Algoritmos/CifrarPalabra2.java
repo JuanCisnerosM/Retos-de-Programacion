701dad40b3db1d6e42c38fb1b33d673c3eac1e17
JuanCisnerosM <juan.cisnerosmtz@hotmail.com>
2025-12-27 11:40:56 -0600

    fix(NombresDeVariables): Correcion de los nombres de las variables en CifrarPalabra2
    se modificio el nombre de las variables para que la metodo principal en ClasePrincipal.java pueda
    inferir el nombre las varuables al llamar al metodo CifrarPalabra2

--- Java/Algoritmos/CifrarPalabra2.java
+++ Java/Algoritmos/CifrarPalabra2.java
@@ -1,24 +1,24 @@
 public class CifrarPalabra2 {
     
     // * Atributos
-    String palabra1;
-    String palabra2;
+    String palabraUno;
+    String palabraDos;
 
     // * Constructor
-    public CifrarPalabra2(String palabra1, String palabra2) {
-        this.palabra1 = palabra1;
-        this.palabra2 = palabra2;
+    public CifrarPalabra2(String palabraUno, String palabraDos) {
+        this.palabraUno = palabraUno;
+        this.palabraDos = palabraDos;
     }
 
     // * Metodo
     public void miCifradoInvertido() {
         String palabraCifrada = "";
-        for (int i = 0; i < palabra1.length() || i < palabra2.length(); i++) {
-            if (i < palabra1.length()) {
-                palabraCifrada += palabra1.charAt(i);
+        for (int i = 0; i < palabraUno.length() || i < palabraDos.length(); i++) {
+            if (i < palabraUno.length()) {
+                palabraCifrada += palabraUno.charAt(i);
             }
-            if (i < palabra2.length()) {
-                palabraCifrada += palabra2.charAt(palabra2.length() - i - 1);
+            if (i < palabraDos.length()) {
+                palabraCifrada += palabraDos.charAt(palabraDos.length() - i - 1);
             }
         }
         System.out.println("La palabra cifrada (en CifrarPalabra2) es: " + palabraCifrada);
